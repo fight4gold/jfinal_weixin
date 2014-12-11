a83074dac826d4e1dc12397a75bbbd47c68ffcbf
unknown <leichentao@YF2-A1310-D.iqiyi.pps>
2014-12-11 13:13:10 +0800

    first commit

---
+++ src/com/jfinal/weixin/sdk/api/ApiConfig.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) 2011-2014, James Zhan 詹波 (jfinal@126.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+
+package com.jfinal.weixin.sdk.api;
+
+/**
+ * 支持多公众账号使用 ThreadLocal<ApiConfig> 与 全局 Interceptor 
+ * 中动态操作此 ThreadLocal 即可
+ */
+public class ApiConfig {
+	
+	private static String url = null;
+	private static String token = null;
+	private static String appId = null;
+	private static String appSecret = null;
+	private static String encodingAesKey = null;
+	private static boolean messageEncrypt = false;	// 消息加密与否
+	
+	// 开发模式将输出消息交互 xml 到控制台
+	private static boolean devMode = false;
+	
+	public static void setDevMode(boolean devMode) {
+		ApiConfig.devMode = devMode;
+	}
+	
+	public static boolean isDevMode() {
+		return devMode;
+	}
+	
+	public static void init(String url, String token) {
+		setUrl(url);
+		setToken(token);
+	}
+	
+	public static void init(String url, String token, String appId, String appSecret) {
+		setUrl(url);
+		setToken(token);
+		setAppId(appId);
+		setAppSecret(appSecret);
+	}
+	
+	public static String getUrl() {
+		if (url == null)
+			throw new RuntimeException("init ApiConfig.setUrl(...) first");
+		return url;
+	}
+	
+	public static void setUrl(String url) {
+		if (url == null)
+			throw new IllegalArgumentException("url can not be null");
+		ApiConfig.url = url;
+	}
+	
+	public static String getToken() {
+		if (token == null)
+			throw new RuntimeException("init ApiConfig.setToken(...) first");
+		return token;
+	}
+	
+	public static void setToken(String token) {
+		if (token == null)
+			throw new IllegalArgumentException("token can not be null");
+		ApiConfig.token = token;
+	}
+	
+	public static String getAppId() {
+		if (appId == null)
+			throw new RuntimeException("init ApiConfig.setAppId(...) first");
+		return appId;
+	}
+	
+	public static void setAppId(String appId) {
+		if (appId == null)
+			throw new IllegalArgumentException("appId can not be null");
+		ApiConfig.appId = appId;
+	}
+	
+	public static String getAppSecret() {
+		if (appSecret == null)
+			throw new RuntimeException("init ApiConfig.setAppSecret(...) first");
+		return appSecret;
+	}
+	
+	public static void setAppSecret(String appSecret) {
+		if (appSecret == null)
+			throw new IllegalArgumentException("appSecret can not be null");
+		ApiConfig.appSecret = appSecret;
+	}
+	
+	public static String getEncodingAesKey() {
+		return encodingAesKey;
+	}
+	
+	public static void setEncodingAesKey(String encodingAesKey) {
+		if (encodingAesKey == null)
+			throw new IllegalArgumentException("encodingAesKey can not be null");
+		ApiConfig.encodingAesKey = encodingAesKey;
+	}
+	
+	public static boolean isEncryptMessage() {
+		return messageEncrypt;
+	}
+	
+	public static boolean getMessageEncrypt() {
+		return messageEncrypt;
+	}
+	
+	/**
+	 *  是否对消息进行加密，对应于微信平台的消息加解密方式：
+	 *  1：true进行加密且必须配置 encodingAesKey
+	 *  2：false采用明文模式，同时也支持混合模式
+	 */
+	public static void setEncryptMessage(boolean messageEncrypt) {
+		ApiConfig.messageEncrypt = messageEncrypt;
+	}
+}
+
+
+
